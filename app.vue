6eb7acd6a29a357276cc5384131d3504e5b6cb2c
xihe <im0k@outlook.com>
2023-11-22 22:09:24 +0800

    chore: 配置基本模块

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import '@unocss/reset/tailwind.css'
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+.dark-mode {
+  background-color: #000;
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+
+const colorMode = useColorMode()
+function toggleDark() {
+  colorMode.value = colorMode.value === 'light'
+    ? 'dark'
+    : 'light'
+}
+</script>
+
+<template>
+  <div text-red>home--{{ x }},{{ y }}
+    <button @click="toggleDark">{{ $colorMode.value }}</button>
+  </div>
+</template>
