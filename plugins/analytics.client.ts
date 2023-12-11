910bdd102f0657418d7f0a40ff90480bd609dac4
xihe <im0k@outlook.com>
2023-12-11 23:34:28 +0800

    Commented out unused imports and function calls

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -1,5 +1,7 @@
-import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+// import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+import { inject } from '@vercel/analytics'
 
 export default defineNuxtPlugin(() => {
-  SpeedInsights()
+  // SpeedInsights()
+  inject()
 })
