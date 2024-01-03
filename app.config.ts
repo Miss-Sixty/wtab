ffdcc700c008210f3ce9997a8ec338f44bba41b0
xihe <im0k@outlook.com>
2024-01-03 22:07:50 +0800

    a

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
 
-// import timezone from 'dayjs/plugin/timezone'
+// import utc from 'dayjs/plugin/utc'
+
+import timezone from 'dayjs/plugin/timezone'
 import pkg from './package.json'
 
-dayjs.extend(utc)
-// dayjs.extend(timezone)
+// dayjs.extend(utc)
+dayjs.extend(timezone)
 const lastBuildTime = ''
 // dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')
 export default defineAppConfig({
