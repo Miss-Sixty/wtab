bb98cfa54fa203c84839ff9772b1fa5cbbf21b7b
xihe <im0k@outlook.com>
2024-01-03 20:56:41 +0800

    a

--- pages/index/calendar.vue
+++ pages/index/calendar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import dayjs from 'dayjs'
+
 // 请求节假日数据
 const { days }: any = await $fetch(`https://cdn.jsdelivr.net/gh/NateScarlet/holiday-cn@master/${2024}.json`)
 </script>
@@ -28,24 +30,27 @@ const { days }: any = await $fetch(`https://cdn.jsdelivr.net/gh/NateScarlet/holi
         </thead>
         <tbody class="divide-y divide-gray-200">
           <tr v-for="(day, i) in days" :key="i">
-            <td class="whitespace-nowrap py-2.5 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+            <td
+              class="whitespace-nowrap py-2.5 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0"
+              :class="dayjs(day.date).isBefore(dayjs()) ? 'line-through' : ''"
+            >
               {{ day.name }}
             </td>
-            <td class="whitespace-nowrap px-3 py-2.5 text-sm text-gray-500">
+            <td
+              class="whitespace-nowrap px-3 py-2.5 text-sm text-gray-500"
+              :class="dayjs(day.date).isBefore(dayjs()) ? 'line-through' : ''"
+            >
               {{ day.date }}
             </td>
             <td class="whitespace-nowrap px-3 py-2.5 text-sm text-gray-500">
               <span
-                v-if="day.isOffDay" class="inline-flex items-center rounded-md bg-primary-50 px-2 py-1 text-xs
-                 text-primary ring-1 ring-inset ring-primary-600/20"
+                class="inline-flex items-center rounded-md  px-2 py-1 text-xs ring-1 ring-inset"
+                :class="[
+                  dayjs(day.date).isBefore(dayjs()) ? 'line-through' : '',
+                  day.isOffDay ? 'bg-primary-50 text-primary ring-primary-600/20' : 'bg-red-50 text-red ring-red-600/20',
+                ]"
               >
-                放假
-              </span>
-              <span
-                v-else class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs
-                 text-red ring-1 ring-inset ring-red-600/20"
-              >
-                上班
+                {{ day.isOffDay ? '放假' : '上班' }}
               </span>
             </td>
           </tr>
