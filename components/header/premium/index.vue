ed5b16845ffac27d163a44ae0c4093d8f52f1da0
xihe <im0k@outlook.com>
2024-01-28 16:32:55 +0800

    a

--- components/header/premium/index.vue
+++ components/header/premium/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxtLink mr-1 animate-heart-beat to="/premium">
+  <nuxtLink mx-1 animate-heart-beat to="/premium">
     <button type="button" hover:text-yellow-400 flex items-center text-yellow-500 text-2xl>
       <div i-solar-crown-bold-duotone /><span text-base>高级用户</span>
     </button>
