f81589dc10de262ab94ed0d3647cc28cc2136dce
xihe <im0k@outlook.com>
2024-02-02 23:31:19 +0800

    a

--- widgets/externalLink/index.vue
+++ widgets/externalLink/index.vue
@@ -24,6 +24,7 @@ const dialogSettingVisible = ref(false)
 
 const widgetData = computed(() => props.widget.widgetData || {})
 function toUrl() {
+  if (props.type||(!widgetData.value.iconUrl && !widgetData.value.iconName)) return
   if (props.dragging) return
   if (props.type === 'del') return dialogSettingVisible.value = true
   const { url, host } = widgetData.value
@@ -36,15 +37,14 @@ const iconNameLength = computed(() => widgetData.value.iconName?.length || 0)
 <template>
   <div :title="widgetData.name" rounded-lg :style="{
     backgroundColor: widgetData.iconType === 'text' ? widgetData.iconBgColor : '',
-    color: widgetData.iconType === 'text' ? '#fff' : ''
-  }" @click="toUrl" p1.5 cursor-pointer flex="~ items-center justify-center">
+  }" @click="toUrl" p1.5 cursor-pointer flex="~ items-center justify-center" bg-white>
     <button v-if="!widgetData.iconUrl && !widgetData.iconName" w-full h-full bg-transparent
       @click="dialogSettingVisible = true">
       <div m-auto class="h-2/5 w-2/5" i-solar-add-square-linear />
     </button>
     <img v-else-if="widgetData.iconType === 'online'" select-none draggable="false" w-full h-full
       :src="widgetData.iconUrl" alt="">
-    <span v-else-if="widgetData.iconType === 'text'" truncate :class="[
+    <span v-else-if="widgetData.iconType === 'text'" truncate text-white :class="[
       iconNameLength === 1 ? 'text-xl' : '',
       iconNameLength < 4 ? 'text-base' : '',
       iconNameLength === 4 ? 'text-sm' : '',
