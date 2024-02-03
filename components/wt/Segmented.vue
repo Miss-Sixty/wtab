332dbacaad9496a9537569732e29fe4471479cb8
xihe <im0k@outlook.com>
2024-02-04 00:53:06 +0800

    a

--- components/wt/Segmented.vue
+++ components/wt/Segmented.vue
@@ -3,58 +3,56 @@ import {
   RadioGroup,
   RadioGroupOption,
 } from '@headlessui/vue'
-const emit = defineEmits(['change'])
-defineProps({
+const emit = defineEmits(['change', 'update:modelValue'])
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
   list: {
     type: Array,
     default: () => [],
   }
 })
-const modelValue = defineModel()
-const labelRef = ref()
-const clientWidth = ref(0)
-const offsetLeft = ref(0)
-const isInit = ref(false)
 
-const onActive = (ev, item) => {
-  clientWidth.value = ev.target.clientWidth;
-  offsetLeft.value = ev.target.offsetLeft;
-  emit('change', item.value);
-}
-
-async function init() {
-  const [firstEl] = labelRef.value
-  clientWidth.value = firstEl.$el.clientWidth;
-  offsetLeft.value = firstEl.$el.offsetLeft;
-  isInit.value = true;
-}
-
-
-const radioGroupRef = ref()
-useIntersectionObserver(
-  radioGroupRef,
-  ([{ isIntersecting }]) => {
-    if (isIntersecting) init()
+const modelValue = computed({
+  get() {
+    return props.modelValue
   },
-)
+  set(val) {
+    emit('update:modelValue', val)
+  }
+})
+const index = computed(() => props.list.findIndex((item) => item.value === modelValue.value))
+const activeIndex = ref(index.value === -1 ? -1.1 : index.value)
+const beforeWidth = computed(() => (1 / props.list.length) * 100 + '%')
 </script>
 
 <template>
-  <RadioGroup ref="radioGroupRef" v-model="modelValue" p0.5 h7 bg-gray-200 rounded-md text-sm>
-    <div relative flex="~ items-center" h-full>
-      <span v-if="isInit" bg-primary rounded h-full absolute left-0 top-0 transition-all :style="{
-        width: clientWidth + 'px',
-        transform: `translateX(${offsetLeft}px)`,
-      }" />
-      <RadioGroupOption ref="labelRef" v-for="item in list" v-slot="{ disabled, checked }" :disabled="item.disabled"
-        :key="item.value" :value="item.value" @click="onActive($event, item)">
-        <button transition px2 relative :class="{
+  <div inline-flex class="radio-group" p0.5 h7 bg-gray-200 rounded-md text-sm>
+    <RadioGroup v-model="modelValue" grid grid-flow-col auto-cols-fr relative class="radio-group-box 
+      before:bg-primary before:rounded before:transition-all">
+      <RadioGroupOption v-for="(item, i) in list" v-slot="{ disabled, checked }" :disabled="item.disabled"
+        :key="item.value" :value="item.value" as="label">
+        <input type="radio" sr-only>
+        <button truncate transition px2 relative size-full rounded :class="{
           'text-white': checked,
           'disabled:opacity-50 dark:disabled:opacity-40 disabled:cursor-not-allowed': disabled
-        }" :disabled="disabled">
+        }" :disabled="disabled" @click="activeIndex = i">
           {{ item.label }}
         </button>
       </RadioGroupOption>
-    </div>
-  </RadioGroup>
+    </RadioGroup>
+  </div>
 </template>
+
+<style scoped>
+.radio-group :deep(.radio-group-box::before) {
+  content: '';
+  height: 100%;
+  position: absolute;
+  pointer-events: none;
+  width: v-bind(beforeWidth);
+  translate: calc(v-bind(activeIndex) * 100%) 0;
+}
+</style>
