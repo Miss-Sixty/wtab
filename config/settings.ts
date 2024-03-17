0d433935d590bdb6f80ae6a2db5ced458ee8f972
xihe <im0k@outlook.com>
2024-03-17 16:38:23 +0800

    a

--- config/settings.ts
+++ config/settings.ts
@@ -21,7 +21,52 @@ export default {
     baseSize: 74, // 卡片大小
     baseMargin: 18, // 卡片间距
     defaultLayout: [
-      // { id: 'date-TAx3EA9AbFy-zaGoljBv6', widgetData: {}, widgetSize: ':2', position: { 4: [0, 0], 6: [0, 0], 8: [0, 0], 12: [0, 0], 14: [0, 0], 16: [0, 0], 18: [0, 0], 20: [0, 0] }, update: 1703935954043, component: 'date' }
-    ],
+      [
+        {
+          "id": "date-dQpK3-WvyDchPAuxxkR6u",
+          "widgetData": {
+            "size": ":2",
+            "singleRow": true
+          },
+          "shadow": false,
+          "widgetSize": ":2",
+          "position": {
+            "4": [
+              0,
+              0
+            ],
+            "6": [
+              0,
+              0
+            ],
+            "8": [
+              0,
+              0
+            ],
+            "12": [
+              0,
+              0
+            ],
+            "14": [
+              0,
+              0
+            ],
+            "16": [
+              0,
+              0
+            ],
+            "18": [
+              0,
+              0
+            ],
+            "20": [
+              0,
+              0
+            ]
+          },
+          "update": 1710664459997,
+          "component": "date"
+        }
+      ]],
   },
 }
