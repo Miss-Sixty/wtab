2bb50f3f81d93dd98ca33ef04ecff8a6daee3f33
xihe <im0k@outlook.com>
2024-03-22 20:46:16 +0800

    a

--- composables/useDownload.ts
+++ composables/useDownload.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs'
 import { parse, stringify } from 'zipson'
 import { decrypt, encrypt } from '@/utils/crypto'
-import { zipSync, strToU8 ,unzlibSync} from 'fflate';
+import { zipSync, strToU8, unzipSync } from 'fflate';
 // 下载配置
 export function downloadConfig(data: any) {
   const brotliData = stringify(data)
@@ -14,37 +14,45 @@ export function downloadConfig(data: any) {
   // link.click()
   // link.remove()
   // URL.revokeObjectURL(url)
-console.log(111,data);
+  console.log(111, data);
 
   let reader = new FileReader();
   // reader.readAsArrayBuffer(imgUrl);
-console.log(22,reader);
-// reader.addEventListener('load', (e) => {
-    const zipped = zipSync({
-      // "imgs": {
-      //   "11.png": new Uint8Array(e?.target?.result),
-      // },
-      "layout.json": strToU8(brotliData)
-    },
-      { level: 1 });
-      console.log(33,zipped);
+  console.log(22, reader);
+  // reader.addEventListener('load', (e) => {
+  const zipped = zipSync({
+    // "imgs": {
+    //   "11.png": new Uint8Array(e?.target?.result),
+    // },
+    "layout.json": strToU8(brotliData)
+  },
+    { level: 9 });
+  console.log(33, zipped);
 
-    const blob = new Blob([zipped.buffer], { type: 'application/zip' });
-    const url = URL.createObjectURL(blob)
-    const link = document.createElement('a')
-    link.href = url
-    link.download = `WTab备份-${dayjs().format('YYYY-MM-DD HH:mm')}.wtab` // 设置保存的文件名称
-    link.click()
-    link.remove()
-    URL.revokeObjectURL(url)
+  const blob = new Blob([zipped.buffer], { type: 'application/zip' });
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement('a')
+  link.href = url
+  link.download = `WTab备份-${dayjs().format('YYYY-MM-DD HH:mm')}.wtab` // 设置保存的文件名称
+  link.click()
+  link.remove()
+  URL.revokeObjectURL(url)
   // });
 }
 
 // 上传配置
-export function uploadConfig(file: any): any {
-  console.log(33,file);
-  const data = unzlibSync(file)
-  console.log(44,data);
+export async function uploadConfig(file: any) {
+  console.log(33, file);
+  const fileData: any = await readFile(file);
+  console.log(44, fileData);
+  const unzip = unzipSync(fileData)
+  console.log(55, unzip);
+
+  const files = []
+  Object.keys(unzip).forEach((key) => {
+    files.push({ name: key, data: unzip[key] });
+  });
+  console.log(66, files);
   // return new Promise((resolve, reject) => {
   //   const reader = new FileReader()
   //   reader.readAsText(file)
@@ -55,4 +63,16 @@ export function uploadConfig(file: any): any {
   //     reject(e)
   //   }
   // })
+}
+
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onerror = reject;
+    reader.onloadend = () => {
+      resolve(new Uint8Array(reader.result));
+    }
+  });
 }
