e574e933bbe1b534bba73695063d15dc26e2dcc5
xihe <im0k@outlook.com>
2024-03-24 02:32:21 +0800

    a

--- components/layout/HomeTetris.vue
+++ components/layout/HomeTetris.vue
@@ -1,4 +1,4 @@
 <template>
-  <NuxtImg quality="100" format="webp" src="/bg/1.heic" object-cover size-full select-none z--1 fixed
+  <NuxtImg quality="100" format="webp" src="/bg/1.heic" object-cover width="100%" height="100%" select-none z--1 fixed
     :placeholder="[500, 250, 90, 9]" />
 </template>
