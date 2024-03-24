f03db62be5d5b8dbba1cdf980abebfbb9e3d2d25
xihe <im0k@outlook.com>
2024-03-24 19:46:23 +0800

    a

--- components/header/index.vue
+++ components/header/index.vue
@@ -34,7 +34,7 @@ const opacityClass = computed(() => ([
 </script>
 
 <template>
-  <nav rounded-lg z-1 p="x2 y1.5" flex items-center gap-1.5 class="pt-[env(safe-area-inset-top, titlebar-area-y)]">
+  <nav rounded-lg z-1 p="x2 pb1.5" flex items-center gap-1.5 class="pt-[env(safe-area-inset-top, titlebar-area-height, 6px)]">
     <!-- <NuxtLink v-for="(item, i) in pageMenu" :key="i" :to="item.to" exact-active-class="text-violet-500">
       <WtIcon :icon="item.icon">
         {{ item.text }}
